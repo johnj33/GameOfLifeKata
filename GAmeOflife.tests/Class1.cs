4db7e3a27ec497a47758ceb4d49c9e1be2072244
John Jackson <john.jackson@advancedcomputersoftware.com>
2016-02-11 17:48:17 +0000

    code from dojo

---
+++ GAmeOflife.tests/Class1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GAmeOflife.tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        const bool LIVE = true;
+        const bool DEAD = false;
+        [Test]
+        public void VerifyAllDeadREturnsAllDead()
+        {
+            var gameOfLife = new GameOfLife.GameOfLife();
+            var input = new List<List<bool>>
+            {
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}
+            };
+
+            var returnedGrid = gameOfLife.ProcessGrid(input);
+
+            Assert.That(returnedGrid, Is.EqualTo(input));
+
+
+        }
+
+        [Test]
+        public void VerifyLiveCellWithLessThen2LiveNeighboursDies()
+        {
+            var gameOfLife = new GameOfLife.GameOfLife();
+            var input = new List<List<bool>>
+            {
+                new List<bool> {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}
+            };
+
+            var expected = new List<List<bool>>
+            {
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}
+            };
+
+            var returnedGrid = gameOfLife.ProcessGrid(input);
+
+            Assert.That(returnedGrid, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void VerifyLiveCellWithMoreThan3LiveNeighboursDies()
+        {
+            var gameOfLife = new GameOfLife.GameOfLife();
+            var input = new List<List<bool>>
+            {
+                new List<bool> {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, LIVE, LIVE, LIVE, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}
+            };
+
+            var expected = new List<List<bool>>
+            {
+                new List<bool> {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, LIVE, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, LIVE, DEAD, DEAD, DEAD, DEAD, DEAD},
+                new List<bool> {DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD, DEAD}
+            };
+
+            var returnedGrid = gameOfLife.ProcessGrid(input);
+
+            Assert.That(returnedGrid, Is.EqualTo(expected));
+        }
+
+
+    }
+}
---
+++ GameOfLife/GameOfLife.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.Data;
+
+namespace GameOfLife
+{
+    public class GameOfLife
+    {
+        const bool ALIVE = true;
+        const bool DEAD = false;
+
+        public List<List<bool>> ProcessGrid(List<List<bool>> grid)
+        {
+            var columnCount = grid.Count;
+            for (var columnNo = 0; columnNo < columnCount; columnNo++)
+            {
+                var rowCount = grid[columnNo].Count;
+                for (var rowNo = 0; rowNo < rowCount; rowNo++)
+                {
+
+                    var liveCount = GetLiveCountForCell(grid, columnNo, rowNo, rowCount, columnCount);
+                    if (grid[columnNo][rowNo] == ALIVE)
+                    {
+                        if (liveCount < 2 || liveCount > 3)
+                        {
+                            grid[columnNo][rowNo] = DEAD;
+                        }
+                    }
+                }
+            }
+            return grid;
+
+        }
+
+        private static int GetLiveCountForCell(List<List<bool>> grid, int columnNo, int rowNo, int rowCount, int columnCount)
+        {
+
+            var numberAlive = 0;
+            var onTopRow = rowNo == 0;
+            var onBottomRow = rowNo == rowCount-1;
+            var onLeftEdge = columnNo == 0;
+            var onRightEdge = columnNo == columnCount-1;
+
+
+            if (!onTopRow)
+            {
+                if (grid[columnNo][rowNo - 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+            if (!onBottomRow)
+            {
+                if (grid[columnNo][rowNo + 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+
+            if (!onLeftEdge)
+            {
+                if (grid[columnNo - 1][rowNo] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+            if (!onRightEdge)
+            {
+                if (grid[columnNo + 1][rowNo] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+
+            if (!onLeftEdge && !onTopRow)
+            {
+                if (grid[columnNo - 1][rowNo - 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+
+            if (!onRightEdge && !onTopRow)
+            {
+                if (grid[columnNo + 1][rowNo - 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+
+            if (!onLeftEdge && !onBottomRow)
+            {
+                if (grid[columnNo - 1][rowNo + 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+
+            if (!onRightEdge && !onBottomRow)
+            {
+                if (grid[columnNo + 1][rowNo + 1] == ALIVE)
+                {
+                    numberAlive += 1;
+                }
+            }
+            return numberAlive;
+
+
+        }
+    }
+}
